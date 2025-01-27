04a5e4aed5ef88aa523d7ffff25a244d165b8bf2
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-27 11:50:13 +0530

    Add files via upload

---
+++ siliconapp.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.header("THIS IS MY FIRST APPLICATION")
+col1=st.columns([1,1])
+with col1[0]:
+   
+    uname = st.text_input("Enter username here:")
+    pswd  = st.text_input("Enter password here:")
+    if st.button("Login"):
+        if uname == "shankar" and pswd=="shan":
+            st.header("Login success")
+        #    enabl=st.checkbox("Enable Camera?")
+            dat11 = st.camera_input("Your webcame",disabled= False)
+            st.write("You clicked below picture")
+            if dat11 is not None:
+                st.image(dat11)
+                st.download_button(label="Download your pic",data=dat11,file_name="test.png")
+            else:
+                st.warning("NO image")
+        else:
+            st.error("Login failed!",icon="🔥")
+
+    
