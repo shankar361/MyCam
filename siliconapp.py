a0b7b9245ad15b5d8bf85e7e3ff922e76afd55b2
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-30 13:03:14 +0530

    Update siliconapp.py

--- siliconapp.py
+++ siliconapp.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.header("Welcome to my Webcam")
-pic=st.camera_input("Capture your pic")
-if pic is not None:
-    st.image(pic)
+col1,col2 = st.columns(2)
+with col1:
+    st.header("Welcome to my Webcam")
+    enabl =st.checkbox("Enable")
+    pic = st.camera_input("Capture your pic",disabled=not enabl)
+with col2:
+    if pic is not None: 
+        st.header("Here is the stunner:")
+        st.image(pic)
