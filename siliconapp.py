7fe58da6083597db1d9634928ec24c8d8835db9d
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-30 16:13:38 +0530

    Update siliconapp.py

--- siliconapp.py
+++ siliconapp.py
@@ -1,17 +1,23 @@
 import streamlit as st
+from datetime import datetime
 
+tt = datetime.now()
+curTime = str(tt.time())
+st.header("Welcome to my Webcam!")
 col1,col2 = st.columns(2)
 with col1:
-    st.header("Welcome to my Webcam!")
+    uname = st.text_input("Enter your name here:")
+    #pswd  = st.text_input("Enter password here:")  
     enabl = st.checkbox("Enable Camera")
     pic   = st.camera_input("Capture your pic!",disabled = not enabl,
                             label_visibility = "visible",
                             help = "This is a basic Camera")
 with col2:
     if pic is not None: 
-        st.header("Here is your stunner!")
+        st.header(uname+", Here is your stunner!")
+     
         st.image(pic)
         filetype = st.selectbox("Select a file type",options = ("png","jpg","bmp"),
                                 placeholder="Choose a file type")
-        st.download_button("Download Pic",pic,file_name = "yourpic." + filetype)
+        st.download_button("Download Pic",pic,file_name = uname +"."+curTime+"."+ filetype)
    
