586f48029d22ada717abdef94c26cd0dabf24588
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-30 15:31:17 +0530

    Update siliconapp.py

--- siliconapp.py
+++ siliconapp.py
@@ -2,10 +2,16 @@ import streamlit as st
 
 col1,col2 = st.columns(2)
 with col1:
-    st.header("Welcome to my Webcam")
-    enabl =st.checkbox("Enable")
-    pic = st.camera_input("Capture your pic",disabled=not enabl)
+    st.header("Welcome to my Webcam!")
+    enabl = st.checkbox("Enable Camera")
+    pic   = st.camera_input("Capture your pic!",disabled = not enabl,
+                            label_visibility = "visible",
+                            help = "This is a basic Camera")
 with col2:
     if pic is not None: 
-        st.header("Here is the stunner:")
+        st.header("Here is your stunner!")
         st.image(pic)
+        filetype = st.selectbox("Select a file type",options = ("png","jpg","bmp"),
+                                placeholder="Choose a file type")
+        st.download_button("Download Pic",pic,file_name = "yourpic." + filetype)
+   
